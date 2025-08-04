d9e4b6b3c7c4fe6ebce230c1ce78cab1e88b44e8
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-08-04 08:44:36 +0700

    video 7

---
+++ src/video18/video18.js
@@ -0,0 +1,22 @@
+const myAge = 25;
+const myScore = 9.1;
+
+console.log("video 18");
+
+console.log(myAge);
+console.log(myScore);
+
+const name = "hoidanit";
+console.log(name);
+
+const isTuiDepTrai = true;
+
+console.log(isTuiDepTrai);
+
+// undefined
+let bla;
+console.log(bla);
+
+// null
+let testNull = null;
+console.log(testNull);
