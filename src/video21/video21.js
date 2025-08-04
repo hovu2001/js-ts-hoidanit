3548b4e1dcd69c01289e0cdfe228303bfd5841be
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-08-04 09:31:43 +0700

    video 11

---
+++ src/video21/video21.js
@@ -0,0 +1,10 @@
+
+let name = "eric";
+let age = 25;
+
+console.log(name, age);
+
+console.log("Name: " + name + ", Age: " + age);
+
+console.log("Name:", name, ", Age:", age);
+
---
+++ src/video22/video22.js
@@ -0,0 +1,14 @@
+console.log("video 22");
+
+
+
+const name = "hoidanit";
+const age = 25;
+
+
+const introduction = "my name is " + name + " and I'm " + age;
+console.log(introduction);
+
+
+const intro = `my name is ${name} and I'm ${age} `;
+console.log(intro);
