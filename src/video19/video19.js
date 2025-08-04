4bff0af960576892b57fc6e7de845aceb4e7d4dc
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-08-04 08:51:14 +0700

    video 8

---
+++ src/video19/video19.js
@@ -0,0 +1,18 @@
+console.log("video 19");
+
+
+
+// object
+
+const person = {
+    name: "hoidanit",
+    age: 25,
+    address: "hanoi"
+}
+
+console.log(person);
+
+
+// array
+const scores = [10, 3, 4, 5, 7]
+console.log(scores);
